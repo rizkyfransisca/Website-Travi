797898e1dea8d756b0084f9a6d5f799e31ff64b8
Rafly Rhamadhan <raflyrhamadhan02@gmail.com>
2022-04-07 22:21:04 +0700

    Menambahkan view destination dan destination-detail

---
+++ resources/views/destination/destination-detail.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Travel Village</title>
+    <link rel="stylesheet" href="/css/index.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+  </head>
+  <style>
+    .bg-artikel {
+      color: white;
+      background-image: url("/Gambar/artikel-tana-toraja.png");
+      height: 400px;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
+  <body>
+    <!--Navbar Atas-->
+    <div class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid px-4">
+        <a class="travi navbar-brand ms-2" href="">
+          <img
+            src="/Gambar/daun.png"
+            width="30px"
+            class="d-inline-block mb-3"
+            alt=""
+          />
+          Travi
+        </a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item me-3">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link active" href="destination.html">Destination</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="tour_package.html">Tour Package</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="join_us.html">Join Us</a>
+          </li>
+          <li class="nav-item me-2">
+            <a class="nav-link" href="">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="container-fluid bg-artikel"></div>
+    <div class="container" style="margin-top: 1cm">
+      <h2 style="margin-bottom: 20px">Tana Toraja</h2>
+      <p><i>Lokasi : Sulawesi Selatan</i></p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quidem sint sapiente est debitis, incidunt veniam vitae itaque atque. Veniam suscipit doloremque mollitia similique adipisci ea molestias dicta, accusantium qui, dolorem at autem nemo, sapiente ipsa. Neque et nesciunt, tempore placeat consectetur eaque quo in tempora beatae aut. Harum a cum debitis vitae? Deserunt laboriosam quaerat fuga esse molestiae optio neque ad debitis iusto veritatis. Animi, rerum minus libero tempora ipsam laborum placeat delectus pariatur consequuntur, numquam similique ea fugiat nisi sunt perspiciatis autem sequi neque veniam ab. Odio dolores laborum, saepe facere alias dicta excepturi tempore nihil hic voluptatem.</p>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br>
+</html>
---
+++ resources/views/destination/destination.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Travel Village</title>
+    <link rel="stylesheet" href="/css/index.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+  </head>
+  <style>
+    .cardkanan {
+      float: right;
+      margin-right: 30px;
+      margin-bottom: 1cm;
+    }
+    .cardkiri {
+      margin-left: 30px;
+      margin-bottom: 1cm;
+    }
+    .carddes {
+      padding-top: 1cm;
+    }
+    .bg-home-1 {
+      color: black;
+      background-image: url("/Gambar/destinasi.png");
+      height: 100px;
+      background-position: top;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
+  <body>
+    <!--Navbar Atas-->
+    <div class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid px-4">
+        <a class="travi navbar-brand ms-2" href="">
+          <img
+            src="/Gambar/daun.png"
+            width="30px"
+            class="d-inline-block mb-3"
+            alt=""
+          />
+          Travi
+        </a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item me-3">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link active" href="destination.html">Destination</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="event_festival.html">Event & Festival</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="tour_package.html">Tour Package</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="join_us.html">Join Us</a>
+          </li>
+          <li class="nav-item me-2">
+            <a class="nav-link" href="">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="container-fluid bg-home-1"></div>
+    <div class="container mt-4 title-home-1">
+      <h4 class="mb-4">Destination</h4>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="carddes"></div>
+          <div class="card cardkiri" style="width: 30rem;">
+            <img
+              src="/Gambar/image1-destination.png"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">Tana Toraja</h5>
+              <p class="card-text">
+                Tana Toraja adalah ikon budaya dan pariwisata di Propinsi
+                Sulawesi Selatan merupakan salah satu daya tarik industri
+                pariwisata Indonesia, hal ini merupakan ...
+              </p>
+              <a href="artikel-tana-toraja.html" class="" style="float: right;"
+                >Lihat detail</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="carddes"></div>
+          <div class="card cardkanan" style="width: 30rem;">
+            <img
+              src="/Gambar/image1-destination.png"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam iure similique facere adipisci deleniti excepturi
+                nulla expedita quasi veniam! Labore...
+              </p>
+              <a href="" class="" style="float: right;">Lihat detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="carddes"></div>
+          <div class="card cardkiri" style="width: 30rem;">
+            <img
+              src="/Gambar/image1-destination.png"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam iure similique facere adipisci deleniti excepturi
+                nulla expedita quasi veniam! Labore...
+              </p>
+              <a href="" class="" style="float: right;">Lihat detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="carddes"></div>
+          <div class="card cardkanan" style="width: 30rem;">
+            <img
+              src="/Gambar/image1-destination.png"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam iure similique facere adipisci deleniti excepturi
+                nulla expedita quasi veniam! Labore...
+              </p>
+              <a href="" class="" style="float: right;">Lihat detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="carddes"></div>
+          <div class="card cardkiri" style="width: 30rem;">
+            <img
+              src="/Gambar/image1-destination.png"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam iure similique facere adipisci deleniti excepturi
+                nulla expedita quasi veniam! Labore...
+              </p>
+              <a href="" class="" style="float: right;">Lihat detail</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="carddes"></div>
+          <div class="card cardkanan" style="width: 30rem;">
+            <img
+              src="/Gambar/image1-destination.png"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam iure similique facere adipisci deleniti excepturi
+                nulla expedita quasi veniam! Labore...
+              </p>
+              <a href="" class="" style="float: right;">Lihat detail</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
